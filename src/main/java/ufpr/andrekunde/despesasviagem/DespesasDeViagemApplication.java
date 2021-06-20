1d6fb767d8ed173961cf6ad9154e7f3f48f3c564
André Kunde <andre.kunde@zup.com.br>
2021-06-20 15:04:30 -0300

    first commit

---
+++ src/main/java/ufpr/andrekunde/despesasviagem/DespesasDeViagemApplication.java
@@ -0,0 +1,13 @@
+package ufpr.andrekunde.despesasviagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasDeViagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasDeViagemApplication.class, args);
+	}
+
+}
