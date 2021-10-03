c35f3cfb49de81cd1a9f13bd7faaa3af8887bce1
André Kunde <andre.kunde@zup.com.br>
2021-10-02 21:27:41 -0300

    saving travel

--- src/main/java/br/ufpr/andrekunde/travelexpenses/controllers/TravelsController.java
+++ src/main/java/br/ufpr/andrekunde/travelexpenses/controllers/TravelsController.java
@@ -1,12 +1,13 @@
 package br.ufpr.andrekunde.travelexpenses.controllers;
 
+import br.ufpr.andrekunde.travelexpenses.controllers.dto.CreateTravelDTO;
 import br.ufpr.andrekunde.travelexpenses.entities.Travel;
 import br.ufpr.andrekunde.travelexpenses.repositories.TravelsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,25 @@ public class TravelsController {
     private TravelsRepository repository;
 
     @GetMapping
-    public List<Travel> find() {
-        return repository.findAll();
+    public ResponseEntity<List<Travel>> find() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Travel> create(@RequestBody CreateTravelDTO createTravelDTO) {
+        Travel travel = new Travel(
+                "open",
+                createTravelDTO.getOrigin(),
+                createTravelDTO.getDestination(),
+                createTravelDTO.getAmount(),
+                new Date(createTravelDTO.getDepartureYear() - 1900, createTravelDTO.getDepartureMonth() - 1, createTravelDTO.getDepartureDay()),
+                new Date(createTravelDTO.getArrivalYear() - 1900, createTravelDTO.getArrivalMonth() - 1, createTravelDTO.getArrivalDay()),
+                createTravelDTO.getReason(),
+                createTravelDTO.getUser()
+        );
+
+        repository.save(travel);
+
+        return ResponseEntity.ok(travel);
     }
 }
--- src/main/java/br/ufpr/andrekunde/travelexpenses/controllers/dto/CreateTravelDTO.java
+++ src/main/java/br/ufpr/andrekunde/travelexpenses/controllers/dto/CreateTravelDTO.java
@@ -4,10 +4,55 @@ import br.ufpr.andrekunde.travelexpenses.entities.Users;
 
 public class CreateTravelDTO {
 
-    private String status;
     private String origin;
     private String destination;
+    private int departureYear, departureMonth, departureDay;
+    private int arrivalYear, arrivalMonth, arrivalDay;
     private Double amount;
     private String reason;
     private Users user;
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public int getDepartureYear() {
+        return departureYear;
+    }
+
+    public int getDepartureMonth() {
+        return departureMonth;
+    }
+
+    public int getDepartureDay() {
+        return departureDay;
+    }
+
+    public int getArrivalYear() {
+        return arrivalYear;
+    }
+
+    public int getArrivalMonth() {
+        return arrivalMonth;
+    }
+
+    public int getArrivalDay() {
+        return arrivalDay;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public Users getUser() {
+        return user;
+    }
 }
